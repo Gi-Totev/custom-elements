ddba9fbc79afe76dd164d57af1b7452c0176af85
Gi-Totev <gi000totv@gmail.com>
2023-05-24 20:27:01 +0300

    Add jsconfig.json

--- accordion-item/accordion-item.js
+++ accordion-item/accordion-item.js
@@ -4,15 +4,19 @@ if (!customElements.get('accordion-item')) {
       super();
     }
 
+    /**
+     * @param {MouseEvent} event - Click event
+     */
     toggle(event) {
       event.preventDefault();
       this.classList.toggle('is-active');
     }
 
     connectedCallback() {
+      /** @type {HTMLElement | null} */
       this.head = this.querySelector('.js-accordion-head');
 
-      this.head.addEventListener('click', this.toggle.bind(this));
+      if(this.head) this.head.addEventListener('click', this.toggle.bind(this));
     }
   }
   customElements.define('accordion-item', AccordionItem);
