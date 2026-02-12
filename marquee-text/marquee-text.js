cf9e81d7acd27da3d6caaa5478466264f92430c2
shopifydev-gt <gi000totv@gmail.com>
2026-02-12 21:25:20 +0200

    Refactor marquee-text

--- marquee-text/marquee-text.js
+++ marquee-text/marquee-text.js
@@ -1,18 +1,14 @@
-if (!customElements.get('marquee-text')) {
+if (!customElements.get("marquee-text")) {
   class MarqueeText extends HTMLElement {
     constructor() {
       super();
-      this.containerLeft = this.parentElement.getBoundingClientRect().left;
-      this.currentLeft = 0;
-      this.stopped = false;
-      this.speed = Number(this.dataset.speed) || 1;
-      this.enablePause = this.hasAttribute('data-pause-on-hover');
     }
 
     animationLoop() {
-      if (this.enablePause && this.stopped) return window.requestAnimationFrame(() => { this.animationLoop(); });
+      if (this.enablePause && this.stopped)
+        return window.requestAnimationFrame(() => this.animationLoop());
 
-      const firstListItem = this.querySelector('*:first-child');
+      const firstListItem = this.querySelector("*:first-child");
       let rightSideOfFirstItem = firstListItem.getBoundingClientRect().right;
 
       if (rightSideOfFirstItem <= this.containerLeft) {
@@ -23,16 +19,27 @@ if (!customElements.get('marquee-text')) {
       this.style.marginLeft = `${this.currentLeft}px`;
       this.currentLeft -= this.speed;
 
-      window.requestAnimationFrame(() => { this.animationLoop(); });
+      window.requestAnimationFrame(() => {
+        this.animationLoop();
+      });
+    }
+
+    initialize() {
+      this.containerLeft = this.parentElement.getBoundingClientRect().left;
+      this.currentLeft = 0;
+      this.stopped = false;
+      this.speed = Number(this.dataset.speed) || 1;
+      this.enablePause = this.hasAttribute("data-pause-on-hover");
     }
 
     connectedCallback() {
-      this.addEventListener('mouseenter', () => this.stopped = true);
-      this.addEventListener('mouseleave', () => this.stopped = false);
+      this.addEventListener("mouseenter", () => (this.stopped = true));
+      this.addEventListener("mouseleave", () => (this.stopped = false));
 
+      this.initialize();
       this.animationLoop();
     }
   }
 
-  customElements.define('marquee-text', MarqueeText);
+  customElements.define("marquee-text", MarqueeText);
 }
